281d2d40cdd225809ba10be8c023fa0a507aefd5
Brad Wang <xwang@ptc.com>
2016-11-14 16:45:23 -0500

    Server Re-organization

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
-/*https://scotch.io/tutorials/how-to-deploy-a-node-js-app-to-heroku
 var express = require('express');
+
 var app = express();
-
-app.use(express.static('root'));
-
 var port = process.env.PORT || 8080;
 
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
-})*/
-
-var express = require('express');
-var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/root'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
+app.get('/', function(req, res){
     res.render('index');
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Server running on port ' + port);
 });
+
+// Tutorial => https://scotch.io/tutorials/how-to-deploy-a-node-js-app-to-
